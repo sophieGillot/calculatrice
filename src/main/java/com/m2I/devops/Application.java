ed91a377194544548bccadee71b41760f00e9efc
Sophie Gillot <Sophie.Gillot@orange.fr>
2021-04-08 10:44:28 +0200

    Ajout sub

--- src/main/java/com/m2I/devops/Application.java
+++ src/main/java/com/m2I/devops/Application.java
@@ -4,7 +4,10 @@ public class Application {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-
+		
+		Calculator c = new Calculator();
+		c.add(2, 3);
+		c.sub(9, 2);
 	}
 
 }
