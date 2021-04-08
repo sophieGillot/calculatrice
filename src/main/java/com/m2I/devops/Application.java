7de6d8c016ca0a5491a69e342f876da77ae53b45
Sophie Gillot <Sophie.Gillot@orange.fr>
2021-04-08 16:17:36 +0200

    indentation

--- src/main/java/com/m2I/devops/Application.java
+++ src/main/java/com/m2I/devops/Application.java
@@ -2,13 +2,14 @@ package com.m2I.devops;
 
 public class Application {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
 		
-		Calculator c = new Calculator();
-		System.out.println(c.add(2, 3));
-		int r=c.sub(9, 2);
-		System.out.println(r);
-	}
+    Calculator c = new Calculator();
+    System.out.println(c.add(2, 3));
+    
+    int r = c.sub(9, 2);
+    System.out.println(r);
+  }
 
 }
--- src/main/java/com/m2I/devops/Calculator.java
+++ src/main/java/com/m2I/devops/Calculator.java
@@ -3,19 +3,19 @@ package com.m2I.devops;
 public class Calculator {
 
 	public int add(int a, int b) {
-		return a+b ;
+		return a + b ;
 	}
 	
 	public int sub(int a, int b) {
-		return a-b ;
+		return a - b ;
 	}
 	
 	public int multi(int a, int b) {
-		return a*b ;
+		return a * b ;
 	}
 	
 	public int div(int a, int b) {
-		return a/b ;
+		return a / b ;
 	}
 	
 	public boolean isPair(int a) {
