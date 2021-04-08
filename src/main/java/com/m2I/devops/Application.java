eaae3e70aad89dceb76ae366773ee33248f66e38
Sophie Gillot <Sophie.Gillot@orange.fr>
2021-04-08 16:39:59 +0200

    correction pmd

--- src/main/java/com/m2I/devops/Application.java
+++ src/main/java/com/m2I/devops/Application.java
@@ -4,10 +4,10 @@ public class Application {
 
   public static void main(String[] args) {
     System.out.println("Hello world!");
-		
+
     Calculator c = new Calculator();
     System.out.println(c.add(2, 3));
-    
+
     int r = c.sub(9, 2);
     System.out.println(r);
   }
--- src/main/java/com/m2I/devops/Calculator.java
+++ src/main/java/com/m2I/devops/Calculator.java
@@ -2,24 +2,24 @@ package com.m2I.devops;
 
 public class Calculator {
 
-	public int add(int a, int b) {
-		return a + b ;
-	}
+  public int add(int a, int b) {
+    return a + b ;
+  }
 	
-	public int sub(int a, int b) {
-		return a - b ;
-	}
+  public int sub(int a, int b) {
+    return a - b ;
+  }
 	
-	public int multi(int a, int b) {
-		return a * b ;
-	}
+  public int multi(int a, int b) {
+    return a * b ;
+  }
 	
-	public int div(int a, int b) {
-		return a / b ;
-	}
+  public int div(int a, int b) {
+    return a / b ;
+  }
 	
-	public boolean isPair(int a) {
-		return (a % 2) == 0 ;
-	}
+  public boolean isPair(int a) {
+    return (a % 2) == 0 ;
+  }
 	
 }
