ee71867a56312e17f955c9eb3bbfebcbaaca40b4
Sophie Gillot <Sophie.Gillot@orange.fr>
2021-04-08 17:27:02 +0200

    Add cpd

--- src/main/java/com/m2I/devops/Application.java
+++ src/main/java/com/m2I/devops/Application.java
@@ -1,14 +1,23 @@
 package com.m2I.devops;
 
+
+/**
+ * Main of calculator engine
+ * 
+ * @author Administrateur
+ * @version
+ */
 public class Application {
+    
+//  private Logger logger=  Logger.getLogger(Application.class.getName();
 
   public static void main(String[] args) {
     System.out.println("Hello world!");
 
-    Calculator c = new Calculator();
+    final Calculator c = new Calculator();
     System.out.println(c.add(2, 3));
 
-    int r = c.sub(9, 2);
+    final int r = c.sub(9, 2);
     System.out.println(r);
   }
 
--- src/main/java/com/m2I/devops/Calculator.java
+++ src/main/java/com/m2I/devops/Calculator.java
@@ -1,23 +1,60 @@
 package com.m2I.devops;
 
+/**
+ * Calculator engine class.
+ * 
+ * 
+ * @author Administrateur
+ * @version 1.0
+ *
+ */
 public class Calculator {
+  
+    /**
+   * 
+   * @param valeur1.
+   * @param valeur2.
+   * @return
+   */
+  public int add(int valeur1, int valeur2) {
+    return valeur1 + valeur2;
+  }
+  
+  /**
+   * 	
+   * @param valeur1.
+   * @param valeur2.
+   * @return
+   */
+  public int sub(int valeur1, int valeur2) {
+    return valeur1 - valeur2;
+  }
+  
+  /**
+   * 	
+   * @param valeur1.
+   * @param valeur2.
+   * @return
+   */
+  public int multi(int valeur1, int valeur2) {
+    return valeur1 * valeur2;
+  }
 
-  public int add(int a, int b) {
-    return a + b ;
+  /**
+   * 
+   * @param valeur1.
+   * @param valeur2.
+   * @return
+   */
+  public int div(int valeur1, int valeur2) {
+    return valeur1 / valeur2;
   }
 	
-  public int sub(int a, int b) {
-    return a - b ;
-  }
-	
-  public int multi(int a, int b) {
-    return a * b ;
-  }
-	
-  public int div(int a, int b) {
-    return a / b ;
-  }
-	
+  /**
+   * 
+   * @param a
+   * @return
+   */
   public boolean isPair(int a) {
     return (a % 2) == 0 ;
   }
